a311e3fe94f0ea8203b09e4ce705cd7831f9a045
diogodiniz11 <diogogd@al.insper.edu.br>
2025-02-12 00:11:08 -0300

    4

--- exe3/main.c
+++ exe3/main.c
@@ -2,46 +2,44 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_GREEN = 26;
-const int BTN_RED = 28;
-const int LED_RED = 6;
-const int LED_GREEN = 4;
+const int BTN_PIN =26;
+const int  BTN_PIN2 =28;
+const int  LED_PIN= 6;
+const int  LED_PIN2= 4;
 
 int main() {
     stdio_init_all();
 
-    bool led_red_state = false;
-    bool led_green_state = false;
+    bool estado1 = false;
+    bool estado2 = false;
 
-    gpio_init(BTN_RED);
-    gpio_set_dir(BTN_RED, GPIO_IN);
-    gpio_pull_up(BTN_RED);
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
 
-    gpio_init(BTN_GREEN);
-    gpio_set_dir(BTN_GREEN, GPIO_IN);
-    gpio_pull_up(BTN_GREEN);
+    gpio_init(BTN_PIN2);
+    gpio_set_dir(BTN_PIN2, GPIO_IN);
+    gpio_pull_up(BTN_PIN2);
 
-    gpio_init(LED_RED);
-    gpio_set_dir(LED_RED, GPIO_OUT);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    gpio_init(LED_GREEN);
-    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_init(LED_PIN2);
+    gpio_set_dir(LED_PIN2, GPIO_OUT);
 
     while (true) {
-        if (!gpio_get(BTN_RED)) {
+        if (!gpio_get(BTN_PIN)) {
             sleep_ms(50);
-            while (!gpio_get(BTN_RED)); 
-            led_red_state = !led_red_state;
-            gpio_put(LED_RED, led_red_state);
-            printf("Botão VERDE pressionado, LED VERDE: %d\n", led_red_state);
+            while (!gpio_get(BTN_PIN)); 
+            estado1 = !estado1;
+            gpio_put(LED_PIN, estado1);
         }
         
-        if (!gpio_get(BTN_GREEN)) {
+        if (!gpio_get(BTN_PIN2)) {
             sleep_ms(50);
-            while (!gpio_get(BTN_GREEN));
-            led_green_state = !led_green_state;
-            gpio_put(LED_GREEN, led_green_state);
-            printf("Botão VERMELHO pressionado, LED VERMELHO: %d\n", led_green_state);
+            while (!gpio_get(BTN_PIN2));
+            estado2 = !estado2;
+            gpio_put(LED_PIN2, estado2);
         }
         
         sleep_ms(10);
--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,51 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_RED = 5;
+const int LED_BLUE = 8;
+const int LED_ORANGE = 11;
+const int LED_YELLOW = 15;
+const int BTN = 28;
+
+
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_RED);
+  gpio_set_dir(LED_RED, GPIO_OUT);
+
+  gpio_init(LED_BLUE);
+  gpio_set_dir(LED_BLUE, GPIO_OUT);
+
+  gpio_init(LED_ORANGE);
+  gpio_set_dir(LED_ORANGE, GPIO_OUT);
+
+  gpio_init(LED_YELLOW);
+  gpio_set_dir(LED_YELLOW, GPIO_OUT);
+
+
+  gpio_init(BTN);
+  gpio_set_dir(BTN, GPIO_IN);
+  gpio_pull_up(BTN);
+
   while (true) {
-    // Use delay de 300 ms entre os estados!
+    if (!gpio_get(BTN)) {
+        gpio_put(LED_RED,1);
+        sleep_ms(300);
+        gpio_put(LED_RED,0);
+
+        gpio_put(LED_BLUE,1);
+        sleep_ms(300);
+        gpio_put(LED_BLUE,0);
+
+        gpio_put(LED_ORANGE,1);
+        sleep_ms(300);
+        gpio_put(LED_ORANGE,0);
+
+        gpio_put(LED_YELLOW,1);
+        sleep_ms(300);
+        gpio_put(LED_YELLOW,0);
+      }
+    }
   }
-}
+
