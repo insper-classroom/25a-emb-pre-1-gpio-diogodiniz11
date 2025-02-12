94aae11035f10eb7da1df7dc80906b88056b3ced
diogodiniz11 <diogogd@al.insper.edu.br>
2025-02-11 23:01:15 -0300

    3.3

--- exe3/main.c
+++ exe3/main.c
@@ -2,90 +2,48 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_PIN = 26;
-const int BTN_PIN2 =27;
-
-const int LED_PIN = 5;
-const int LED_PIN2 = 6;
-
+const int BTN_PIN =26;
+const int  BTN_PIN2 =27;
+const int  LED_PIN= 5;
+const int  LED_PIN2= 6;
 
 int main() {
-  stdio_init_all();
+    stdio_init_all();
 
-  int contador1 = 1;
-  int contador2 = 1;
+    bool estado1 = false;
+    bool estado2 = false;
 
-  gpio_init(BTN_PIN);
-  gpio_set_dir(BTN_PIN, GPIO_IN);
-  gpio_pull_up(BTN_PIN);
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
 
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init(BTN_PIN2);
+    gpio_set_dir(BTN_PIN2, GPIO_IN);
+    gpio_pull_up(BTN_PIN2);
 
-  gpio_init(LED_PIN2);
-  gpio_set_dir(LED_PIN2, GPIO_OUT);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-  gpio_init(BTN_PIN2);
-  gpio_set_dir(BTN_PIN2, GPIO_IN);
-  gpio_pull_up(BTN_PIN2);
+    gpio_init(LED_PIN2);
+    gpio_set_dir(LED_PIN2, GPIO_OUT);
 
-  while (true) {
-    if (!gpio_get(BTN_PIN)) {
-      contador1 +=1 ;
-
-    while (contador1%2 == 0) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(300);
-
+    while (true) {
+        if (!gpio_get(BTN_PIN)) {
+            sleep_ms(50);
+            while (!gpio_get(BTN_PIN)); 
+            estado1 = !estado1;
+            gpio_put(LED_PIN, estado1);
+        }
+        
         if (!gpio_get(BTN_PIN2)) {
-          contador2 +=1 ;
-
-        while (contador2%2 == 0) {
-            gpio_put(LED_PIN2, 1);
-            sleep_ms(300);
-            if (!gpio_get(BTN_PIN)){
-              gpio_put(LED_PIN, 0);
-              sleep_ms(300);
-              contador1 +=1 ; }
-
-            if (!gpio_get(BTN_PIN2)){
-              gpio_put(LED_PIN2, 0);
-              sleep_ms(300);
-              contador2 +=1 ;}}}
-
-        if (!gpio_get(BTN_PIN)){
-          gpio_put(LED_PIN, 0);
-          sleep_ms(300);
-          contador1 +=1 ; }}}
-
-  if (!gpio_get(BTN_PIN2)) {
-    contador2 +=1 ;
-
-  while (contador2%2 == 0) {
-      gpio_put(LED_PIN2, 1);
-      sleep_ms(300);
-
-      if (!gpio_get(BTN_PIN)) {
-        contador1 +=1 ;
-
-      while (contador1%2 == 0) {
-          gpio_put(LED_PIN, 1);
-          sleep_ms(300);
-          if (!gpio_get(BTN_PIN2)){
-            gpio_put(LED_PIN2, 0);
-            sleep_ms(300);
-            contador2 +=1 ;}
-
-          if (!gpio_get(BTN_PIN)){
-            gpio_put(LED_PIN, 0);
-            sleep_ms(300);
-            contador1 +=1 ;
-          }    
-      }
-      }
-      if (!gpio_get(BTN_PIN2)){
-        gpio_put(LED_PIN2, 0);
-        sleep_ms(300);
-        contador2 +=1 ;}}}}
-      return 0;}
-  
+            sleep_ms(50);
+            while (!gpio_get(BTN_PIN2));
+            estado2 = !estado2;
+            gpio_put(LED_PIN2, estado2);
+        }
+        
+        sleep_ms(10);
+    }
+    
+    return 0;
+}
