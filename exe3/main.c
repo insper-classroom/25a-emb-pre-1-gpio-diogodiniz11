a37cac94c62a964fc01ef78d7d4f8f4f7433852b
diogodiniz11 <diogogd@al.insper.edu.br>
2025-02-11 23:20:22 -0300

    3.5

--- exe3/main.c
+++ exe3/main.c
@@ -2,44 +2,46 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_PIN =26;
-const int  BTN_PIN2 =28;
-const int  LED_PIN= 6;
-const int  LED_PIN2= 4;
+const int BTN_GREEN = 26;
+const int BTN_RED = 28;
+const int LED_RED = 6;
+const int LED_GREEN = 4;
 
 int main() {
     stdio_init_all();
 
-    bool estado1 = false;
-    bool estado2 = false;
+    bool led_red_state = false;
+    bool led_green_state = false;
 
-    gpio_init(BTN_PIN);
-    gpio_set_dir(BTN_PIN, GPIO_IN);
-    gpio_pull_up(BTN_PIN);
+    gpio_init(BTN_RED);
+    gpio_set_dir(BTN_RED, GPIO_IN);
+    gpio_pull_up(BTN_RED);
 
-    gpio_init(BTN_PIN2);
-    gpio_set_dir(BTN_PIN2, GPIO_IN);
-    gpio_pull_up(BTN_PIN2);
+    gpio_init(BTN_GREEN);
+    gpio_set_dir(BTN_GREEN, GPIO_IN);
+    gpio_pull_up(BTN_GREEN);
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
 
-    gpio_init(LED_PIN2);
-    gpio_set_dir(LED_PIN2, GPIO_OUT);
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
 
     while (true) {
-        if (!gpio_get(BTN_PIN)) {
+        if (!gpio_get(BTN_RED)) {
             sleep_ms(50);
-            while (!gpio_get(BTN_PIN)); 
-            estado1 = !estado1;
-            gpio_put(LED_PIN, estado1);
+            while (!gpio_get(BTN_RED)); 
+            led_red_state = !led_red_state;
+            gpio_put(LED_RED, led_red_state);
+            printf("Botão VERDE pressionado, LED VERDE: %d\n", led_red_state);
         }
         
-        if (!gpio_get(BTN_PIN2)) {
+        if (!gpio_get(BTN_GREEN)) {
             sleep_ms(50);
-            while (!gpio_get(BTN_PIN2));
-            estado2 = !estado2;
-            gpio_put(LED_PIN2, estado2);
+            while (!gpio_get(BTN_GREEN));
+            led_green_state = !led_green_state;
+            gpio_put(LED_GREEN, led_green_state);
+            printf("Botão VERMELHO pressionado, LED VERMELHO: %d\n", led_green_state);
         }
         
         sleep_ms(10);
